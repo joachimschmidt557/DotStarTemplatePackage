589c61225b56bbd44fe11ba87c81cf126dbe2150
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-09-21 09:06:04 +0800

    First commit

---
+++ Package.py
@@ -0,0 +1,25 @@
+"""
+The Package.py file contains all necessary information for
+handling this package.
+
+This python script will be called by DotStar when the user
+decides to install, run or do something with the package.
+
+Possible command-line arguments are:
+    - "install"
+    - "run"
+    - "open"
+"""
+
+import sys
+
+action = sys.argv[0]
+if action == "install":
+    install_package()
+
+def install_package():
+    """
+    This function contains all necessary information for
+    installing the package
+    """
+    pass

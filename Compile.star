62e4a1a967586740dc8473d86834de024efcd702
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-09-23 13:20:52 +0800

    Update

---
+++ Compile.star
@@ -0,0 +1,28 @@
+{
+    "Basic Package Information":
+    {
+        "Name": "dotstartemplate"
+    },
+    "Author information":
+    {
+        "Name": "joachimschmidt557",
+        "Company": "",
+        "Website": "https://github.com/joachimschmidt557",
+        "Email": ""
+    },
+    "Application Information": 
+    {
+        "Name": "Test",
+        "Friendly Name": "DotStar",
+        "Description": "A simple testing package to check wheter DotStar works",
+        "Version": "1.0.0",
+        "Version System": "MajorMinor"
+    },
+    "Dependencies":
+    [
+        {
+            "Name": "Python3",
+            "File": "Python3.star"
+        }
+    ]
+}
--- Package.py
+++ Package.py
@@ -7,8 +7,11 @@ decides to install, run or do something with the package.
 
 Possible command-line arguments are:
     - "install"
+        - "win.install"
     - "run"
+        - "win.run"
     - "open"
+    - "compile"
 """
 
 import sys
@@ -18,13 +21,30 @@ def install_package():
     This function contains all necessary information for
     installing the package
     """
-    print("In installing")
+    print("Test install")
+    print("If you see this message, your DotStar is working!")
 
 def run_package():
-    print("In run package")
+    """
+    This function contains all necessary information for
+    running the package
+    """
+    print("Test run")
+    print("If you see this message, your DotStar is working!")
+
+def compile_package():
+    """
+    This function contains all necessary information for
+    compiling the package
+    """
+    print("Test compile")
+    print("If you see this message, your DotStar is working!")
+
 
 action = sys.argv[1]
 if action == "install":
     install_package()
 if action == "run":
     run_package()
+if action == "compile":
+    compile_package()
